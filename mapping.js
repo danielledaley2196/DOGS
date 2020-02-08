91280601b187141ba8fc4bbeada108211cefde8d
A Hoerman <awhoerman@me.com>
2020-02-08 11:12:51 -0500

    fixed png

--- mapping.js
+++ mapping.js
@@ -129,7 +129,7 @@
           // assign a letter of the alphabetic to each marker icon.
           for (var i = 0; i < results.length; i++) {
             var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-            var markerIcon = 'dogPark2.png';
+            var markerIcon = MARKER_PATH + markerLetter + '.png';
             // Use marker animation to drop the icons incrementally on the map.
             markers[i] = new google.maps.Marker({
               position: results[i].geometry.location,
