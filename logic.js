3ceb953e3e6358dcc194fbb786f7d5a336dd42ef
BytteL <lettybedard@gmail.com>
2020-02-08 11:29:24 -0500

    sidebar clicks return locations

--- logic.js
+++ logic.js
@@ -5,52 +5,33 @@ $(function(){
   
   //  DECLARATIONS 
   //let recentPlaces = JSON.parse(localStorage.getItem("recentPlaces"));
-  let thePlaces = [{
-    place: "Boston",
-    latitude: 123,
-    longitude: 456
-  },{
-    place: "Miami",
-    latitude: 321,
-    longitude: 654
-  }];
+  let thePlaces = [
+    "Boston",
+    "Miami",
+    "Portsmouth"
+];
 
   //  REFERENCES
   let $search = $("#search");
-  let $input = $("#input");
-  let $mapSpot = $("#theMap");
+  let $input = $("#autocomplete");
   let $places = $("#popularPlaces");
   let $hamburger = $("#theHamburger");
 
+
   //  FUNCTIONS
-  function mapByCoords(lat, long){
-    //displays map centered on lat, long
-  }
-
-  //displays the google map for the given zip
-  function mapByZip(location){
-    //convert zip to lat long
-    //mapByCoords(lat, long);
-  }
-
-  //displays the google map for the given city
-  function mapByCity(location){
-    //convert city to lat long
-    //mapByCoords(lat, long);
-  }
 
   //empty and draw the sidebar
   function drawPlaces(){
     $places.empty();
+    $hamburger.empty();
     thePlaces.forEach(function(element){
       let thisOne = $("<li>")
                       .addClass("place")
-                      .attr({
-                        "data-lat": element.latitude, 
-                        "data-long": element.longitude
-                      })
-                      .text(element.place)
+                      .text(element);
       $places.append(thisOne);
+      thisOne = $("<li>")
+                      .addClass("place")
+                      .text(element);
       $hamburger.append(thisOne);
     });
   }
@@ -58,36 +39,27 @@ $(function(){
   //  EVENT LISTENERS
 
   //when search is clicked, return the map of that location and add to recent
-  // $(document).on("click", $search, function(){
-  //   let inputString = $input.text.trim();
-  //   $input.text = "";
-  //   if (recentPlaces === null){
-  //     recentPlaces = [inputString];
-  //   }
-  //   else {
-  //     recentPlaces.unshift("inputString");
-  //   }
-
-  //   showRecent();
-
-  //   if ($.isNumeric(inputString)){
-  //     mapByZip(inputString);
-  //   }
-  //   else {
-  //     mapByCity(inputString);
-  //   } 
-  
-  // });
-
-  //when a saved location is clicked, show that location
-  $(document).on("click", $(".place"), function(){
-  //console.log(`${$(this).attr("data-lat")}, ${$(this).attr("data-long")} was clicked`);
-  console.log(this);
-  //  mapByCoords($(this).attr("data-lat"), $(this).attr("data-long"));
+  $(document).on("focusout", $input, function(){
+    $("#dog").css("display", "hidden");
+    let inputString = $input.text.trim();
+    $input.text = "";
+    if (recentPlaces === null){
+      recentPlaces = [inputString];
+    }
+    else {
+      recentPlaces.unshift("inputString");
+    }
+    showRecent();
   });
 
-  //when save is clicked, boot out one of the defaults and add to recent/saved
+  //when a saved location is clicked, show that location
+  $(document).on("click", ".place", function(){
+    console.log(`${$(this).text()} was clicked`);
+    //show that result in the map pane
+  });
+
   //when reset recent is clicked, empty that div and clear local
+
   //when dog div is clicked, change the random dog
 
   drawPlaces();
